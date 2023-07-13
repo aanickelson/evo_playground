0aaa67b2ab1fa2d923f4ba3b18b24c8b82ab65dd
Anna Nickelson <aanickelson@gmail.com>
2023-07-13 14:54:22 -0700

    Reconfigure rover_wrapper.py to be reusable for single or multiagent In the process of updating trained_pol_rollout.py to test multiple agents.

--- learning/rover_wrapper.py
+++ learning/rover_wrapper.py
@@ -11,25 +11,18 @@ from pymap_elites_multiobjective.scripts_data.run_env import run_env
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-class RoverWrapper:
-    def __init__(self, env, param):
-        self.env = env
-        self.p = param
-        self.st_size = env.state_size()
-        self.hid = lp.hid
-        self.act_size = env.action_size()
+class AgPol:
+    def __init__(self, st_size, hid, act_size):
+        self.st_size = st_size
+        self.hid = hid
+        self.act_size = act_size
         self.w0_size = self.st_size * self.hid
         self.w2_size = self.hid * self.act_size
         self.b0_size = self.hid
         self.b2_size = self.act_size
         self.model = NN(self.st_size, self.hid, self.act_size)
-        self.n_evals = 0
-        self.vis = False
-        self.use_bh = True
 
-    def evaluate(self, x):
-        self.env.reset()
-
+    def set_trained_network(self, x):
         # Use these ONLY if you're using the old data (before 7/7/2023)
         # w0_wts = from_numpy(np.reshape(x[:self.w0_size], (self.hid, self.st_size)))
         # w2_wts = from_numpy(np.reshape(x[self.w0_size:], (self.act_size, self.hid)))
@@ -47,8 +40,40 @@ class RoverWrapper:
         self.model.set_biases([b0_wts, b1_wts])
         self.model.set_weights([w0_wts, w2_wts])
 
-        out_vals = run_env(self.env, [self.model], self.p, use_bh=self.use_bh, vis=self.vis)
-        self.n_evals += 1
+
+class RoverWrapper:
+    def __init__(self, env, param):
+        self.env = env
+        self.p = param
+        self.agents = self.setup_ag()
+        self.vis = False
+        self.use_bh = True
+
+    def setup_ag(self):
+        st_size = self.env.state_size()
+        hid = lp.hid
+        act_size = self.env.action_size()
+        ag = []
+        for _ in range(self.p.n_agents):
+            ag.append(AgPol(st_size, hid, act_size))
+        return ag
+
+    def evaluate(self, x):
+        self.env.reset()
+        wts = x
+        if self.p.n_agents == 1 and len(x) > 1:
+            wts = [x]
+        if len(wts) != self.p.n_agents:
+            print(f"given x is of length {len(x)}, should be {self.p.n_agents}")
+            return
+
+        models = []
+        for i, w in enumerate(wts):
+            self.agents[i].set_trained_network(w)
+            models.append(self.agents[i].model)
+
+        out_vals = run_env(self.env, models, self.p, use_bh=self.use_bh, vis=self.vis)
+
 
         if self.use_bh:
             fitness, bh = out_vals
